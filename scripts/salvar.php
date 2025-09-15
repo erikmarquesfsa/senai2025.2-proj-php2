4f9673b02fcc41a59216052c956f63fe81654eb9
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-15 20:18:14 -0300

    d

--- scripts/salvar.php
+++ scripts/salvar.php
@@ -1,9 +1,11 @@
 <?php
 
-$produto = $_POST['produto'];
+$produto = $_POST['n_produto'];
 $categoria = $_POST['categoria'];
 $qtde_estoque = $_POST['qtde_estoque'];
 $valor = $_POST['valor'];
 $validade = $_POST['validade'];
+
+$sql = "INSERT INTO produto values (null,'$produto','$categoria','$qtde_estoque','$valor','$validade')";
 
 ?>
