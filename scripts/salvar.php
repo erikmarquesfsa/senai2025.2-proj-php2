50eeb042bcd344f41374365d48925bd30c2d5187
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-15 20:31:09 -0300

    d

--- scripts/salvar.php
+++ scripts/salvar.php
@@ -1,4 +1,10 @@
+<html>
+    <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>    
 <?php
+
+include "config.php";
 
 $produto = $_POST['n_produto'];
 $categoria = $_POST['categoria'];
@@ -7,5 +13,33 @@ $valor = $_POST['valor'];
 $validade = $_POST['validade'];
 
 $sql = "INSERT INTO produto values (null,'$produto','$categoria','$qtde_estoque','$valor','$validade')";
+$query = $mysqli->query($sql);
 
-?>
+if($query){ ?>
+    <script>
+        Swal.fire({
+        title: "Sucesso!",
+        text: "Cadastro Adicionado",
+        icon: "success"
+        }).then(okay=>{
+            if(okay){
+                window.location.href = "../index.php";
+            }
+        });
+    </script>
+<?php }else{ ?>
+    <script>
+        Swal.fire({
+        title: "Ops!",
+        text: "Houve um erro. Por favor, tente novamente",
+        icon: "error"
+        }).then(okay=>{
+            if(okay){
+                window.location.href = "../index.php";
+            }
+        });
+    </script>
+<?php } ?>
+
+    </body>
+</html>
