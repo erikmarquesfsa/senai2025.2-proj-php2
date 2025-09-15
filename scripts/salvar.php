2dbe891cfc1d69ef5522b6ca43405e643082f0e8
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-15 20:13:24 -0300

    d

--- scripts/salvar.php
+++ scripts/salvar.php
@@ -1,5 +1,9 @@
 <?php
 
-print_r($_POST);
+$produto = $_POST['produto'];
+$categoria = $_POST['categoria'];
+$qtde_estoque = $_POST['qtde_estoque'];
+$valor = $_POST['valor'];
+$validade = $_POST['validade'];
 
 ?>
